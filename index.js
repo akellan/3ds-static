221874470acc8cd19d0a3a4390378d02d306f244
Mykola Kodrul <nikolay.kodrul@gmail.com>
2018-11-27 00:14:31 +0200

    feature: file upload

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "static/");
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+var staticUpload = multer({ storage });
 const fs = require("fs");
 
 require("./models/folder");
@@ -8,6 +20,8 @@ app.set("view engine", "pug");
 app.use("/static", express.static("static"));
 
 const port = 3000;
+
+app.post("/upload", staticUpload.single("game"));
 
 app.get("/", (req, res) => {
   const files = fs.readdirSync("./static");
