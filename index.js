8b4c551ae048314e765ea5037fad0a4ce0c70134
Mykola Kodrul <nikolay.kodrul@gmail.com>
2018-11-26 22:38:00 +0200

    feature: read files from folder and show QR codes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const fs = require("fs");
+
+require("./models/folder");
 const app = express();
 
 app.set("view engine", "pug");
@@ -7,7 +10,10 @@ app.use("/static", express.static("static"));
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+  const files = fs.readdirSync("./static");
+  const address = "http://192.168.0.174:3001/static/";
+
+  res.render("index", { files, address });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
