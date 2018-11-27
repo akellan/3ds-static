b9d3c5dcd5451997f00d0e910173137c2848da49
Mykola Kodrul <nikolay.kodrul@gmail.com>
2018-11-27 21:16:54 +0200

    refactor: extract configuration file

---
+++ server/configuration.js
@@ -0,0 +1,5 @@
+const path = require("path");
+module.exports = {
+  port: 3000,
+  gamesFolder: path.join(__dirname, "static")
+};
--- server/index.js
+++ server/index.js
@@ -4,21 +4,21 @@ const fs = require("fs");
 const cors = require("./middlewares/cors");
 const upload = require("./middlewares/upload");
 const static = require("./middlewares/static");
+const config = require("./configuration");
 
-const gamesFolder = path.join(__dirname, "static");
-const port = 3000;
 const app = express();
+const { gamesFolder, port } = config;
 
 cors.register(app);
 upload.register(app, gamesFolder, "game");
 static.register(app, gamesFolder);
 
 app.get("/files", (req, res) => {
-  const files = fs.readdirSync("./static");
+  const files = fs.readdirSync(gamesFolder);
   const address = `static/`;
 
   const fileAdresses = files.map(filename => address + filename);
   res.send(fileAdresses);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
