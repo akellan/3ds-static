50d1c99720d496cf4b00d30ee2e5b50c88208e57
Mykola Kodrul <nikolay.kodrul@gmail.com>
2018-11-29 00:04:08 +0200

    refactor: move all CSS to JS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { GamesList } from "./games/GamesList";
 import JssProvider from "react-jss/lib/JssProvider";
 import { create } from "jss";
 import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
+import { createGlobalStyle } from "styled-components";
 
 const generateClassName = createGenerateClassName();
 const jss = create({
@@ -10,11 +11,19 @@ const jss = create({
   insertionPoint: document.getElementById("jss-insertion-point")
 });
 
+const RootStyle = createGlobalStyle`
+  body {
+    padding: 0;
+  }
+`;
 class App extends Component {
   render() {
     return (
       <JssProvider jss={jss} generateClassName={generateClassName}>
-        <GamesList />
+        <>
+          <RootStyle />
+          <GamesList />
+        </>
       </JssProvider>
     );
   }
